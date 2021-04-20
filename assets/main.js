e5549376976e71ebebc8c470fa6a789cc541626f
giuseppe <placidagiuseppe@gmail.com>
2021-04-20 17:02:50 +0200

    ...

---
+++ assets/main.js
@@ -0,0 +1,18 @@
+// function men() {
+//   console.log("ciao");
+//   var cliccato = document.getElementById("magico");
+//   if (cliccato.style.display === "block") {
+//       cliccato.style.display = "none";
+//   }else {
+//     cliccato.style.display = "block";
+//   }
+// }
+document.getElementById('menu').addEventListener('click',function() {
+  // var clicccato = document.getElementById("magico");
+  //   if (cliccato.style.display === "block") {
+  //       cliccato.style.display = "none";
+  //   }else {
+  //     cliccato.style.display = "block";
+  //   }
+  console.log("ciao");
+})
